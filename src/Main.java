c56e8365b85ad8b60af8276aff0e4acdd04dfc05
Syat <titmouse99@mail.ru>
2023-05-03 01:31:05 +0300

    3 задача

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,9 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
+
     public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
         int[] arr = new int[30];
@@ -15,22 +17,43 @@ public class Main {
         }
         return arr;
     }
-     public static void task1() {
-         System.out.println("Задача 1");
-         int[] arr = generateRandomArray();
-             int sum = 0;
-             for (int i = 0; i < arr.length; i++) {
-                 sum = sum + arr[i];
-             }
-             System.out.println(" Сумма трат за месяц составила " + sum + " рублей ");
-         }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int[] arr = generateRandomArray();
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+        System.out.println(" Сумма трат за месяц составила " + sum + " рублей ");
+    }
+
     public static void task2() {
         System.out.println("Задача 2");
         int[] arr = generateRandomArray();
-        int[] arr2 = arr.clone();
-        Arrays.sort(arr2);
-        int lastElements = arr2[arr2.length - 1];
-        System.out.println(" Минимальная сумма трат за день составила " + arr2[0] + " рублей. Максимальная сумма трат за день составила " + lastElements + " рублей ");
+        int min = arr[0];
+        for (int num : arr) {
+            if (num < min) {
+                min = num;
+            }
+        }
+        int max = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println(" Минимальная сумма трат за день составила " + min + " рублей. Максимальная сумма трат за день составила " + max + " рублей ");
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        int[] arr = generateRandomArray();
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];}
+            float monthlyAverge = sum / 30;
+            System.out.println(" Средняя сумма трат за месяц составила " + monthlyAverge + " рублей ");
         }
     }
 
